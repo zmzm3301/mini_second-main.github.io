3aee0aacc79030aab5dc93292bb8fca0b22e38be
YeongMin Cho <zmzm3301@naver.com>
2023-01-25 14:21:01 +0900

    gunicorn

--- mini_second/settings.py
+++ mini_second/settings.py
@@ -134,6 +134,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
+
 STATICFILES_DIRS = [BASE_DIR/'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
