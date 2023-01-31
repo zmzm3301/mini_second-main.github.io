4c724dc0bad8cf119fee4fdc40b7a36e26a7b5af
YeongMin Cho <zmzm3301@naver.com>
2023-01-31 12:41:21 +0900

    중간 커밋7

--- webapp/models.py
+++ webapp/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 
+
 class Info(models.Model):
-   battery = models.CharField(max_length=30)
-   color =  models.CharField(max_length=30)
-   runtime = models.CharField(null=True, max_length=30)
+    battery = models.CharField(max_length=30)
+    color = models.CharField(max_length=30)
+    runtime = models.CharField(null=True, max_length=30)
 
-   created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
 
-   updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-   def __str__(self):
-      return f'[{self.pk}] bat : {self.battery}% / runtime : {self.runtime}ms'
-
-
-
-# Create your models here.
+    def __str__(self):
+        return f'[{self.pk}] bat : {self.battery}% / runtime : {self.runtime}ms'
